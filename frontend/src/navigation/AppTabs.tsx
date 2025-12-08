28a7b3541594d3b37e4fce9d3a3f7a1bcf73effa
KaanCaman <106434723+KaanCaman@users.noreply.github.com>
2025-12-08 20:34:09 +0300

    frontend: enhance AppTabs with theme-based styling and icons

--- frontend/src/navigation/AppTabs.tsx
+++ frontend/src/navigation/AppTabs.tsx
@@ -5,21 +5,47 @@ import HomeStack from "./stacks/HomeStack";
 import PaymentStack from "./stacks/PaymentStack";
 import HistoryStack from "./stacks/HistoryStack";
 import ProfileStack from "./stacks/ProfileStack";
+import { useTheme } from "../providers/ThemeProvider";
+import { View, StyleSheet } from "react-native";
+import { HouseIcon } from "phosphor-react-native";
 
 const Tab = createBottomTabNavigator();
 
 export default function AppTabs() {
+  const theme = useTheme();
   return (
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
         tabBarShowLabel: true,
+        tabBarLabelStyle: {},
+        tabBarActiveTintColor: theme.colors.accent.primary,
+        tabBarInactiveTintColor: theme.colors.text.muted,
+        tabBarBackground: () => (
+          <View
+            style={[
+              StyleSheet.absoluteFill,
+              {
+                backgroundColor: theme.colors.background.tertiary,
+              },
+            ]}
+          />
+        ),
       }}
     >
       <Tab.Screen
         name="HomeTab"
         component={HomeStack}
-        options={{ title: "Home" }}
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <HouseIcon
+              color={
+                focused ? theme.colors.accent.primary : theme.colors.text.muted
+              }
+            />
+          ),
+        }}
       />
       <Tab.Screen
         name="PaymentTab"
